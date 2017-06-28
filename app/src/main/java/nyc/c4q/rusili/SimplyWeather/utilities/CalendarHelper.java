b961e5f29d14e85bee65aa00cb6d49bcc815bb13
RusiLi <Rusili56@gmail.com>
2017-06-28 13:22:41 -0400

    Singleton'd CalendarHelper.

---
+++ app/src/main/java/nyc/c4q/rusili/SimplyWeather/utilities/CalendarHelper.java
@@ -0,0 +1,61 @@
+package nyc.c4q.rusili.SimplyWeather.utilities;
+
+import android.util.Log;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CalendarHelper {
+	private static CalendarHelper calendarHelper;
+
+	private CalendarHelper(){}
+
+	public static CalendarHelper getInstance(){
+		if (calendarHelper == null){
+			calendarHelper = new CalendarHelper();
+		}
+		return calendarHelper;
+	}
+
+	public Date getDate(){
+		Date date = new Date();
+		return date;
+	}
+
+	public CharSequence ifSingleDigit (String format) {
+		CharSequence charSequence = format;
+
+		if (Integer.parseInt(format) < 10) {
+			charSequence = String.valueOf(format.charAt(1));
+		}
+		return charSequence;
+	}
+
+	public CharSequence getTwoCharWeekday (String weekdayShort) {
+		CharSequence charSequence = weekdayShort;
+
+		if (weekdayShort.contains("T") || weekdayShort.contains("S")) {
+			charSequence = weekdayShort.substring(0, 2);
+		} else {
+			charSequence = String.valueOf(weekdayShort.charAt(0));
+		}
+		return charSequence;
+	}
+
+	public boolean before30Minutes () {
+		Calendar calendar = Calendar.getInstance();
+		Log.d("Minutes", String.valueOf(calendar.get(Calendar.MINUTE)));
+		if (calendar.get(Calendar.MINUTE) < 30){
+			return true;
+		}
+		return false;
+	}
+
+	public String change24to12hour (String input) {
+		int hour = Integer.parseInt(input);
+		if (hour > 12){
+			return String.valueOf(hour - 12);
+		}
+		return input;
+	}
+}
