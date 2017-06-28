f36537ae651db6d4b5651594b7109d14141fb632
RusiLi <Rusili56@gmail.com>
2017-06-28 13:38:50 -0400

    reworking

---
+++ app/src/main/java/nyc/c4q/rusili/SimplyWeather/network/WUndergroundAPI/WUndergroundRetrofitCall.java
@@ -0,0 +1,11 @@
+package nyc.c4q.rusili.SimplyWeather.network.WUndergroundAPI;
+
+	  import retrofit2.Call;
+	  import retrofit2.http.GET;
+	  import retrofit2.http.Path;
+
+public interface WUndergroundRetrofitCall {
+
+	@GET ("api/{apikey}/conditions/forecast10day/hourly/q/{zipcode}.json")
+	Call <ResponseConditionsForecast10DayHourly> getConditionsForecastHourly (@Path ("apikey") String apikeyParam, @Path ("zipcode") int zipcodeParam);
+}
